f6a45790b44904f00475501281591ef24783f2ce
Mxn-ptr <maxence0@hotmail.fr>
2022-02-14 09:55:17 +0100

    Fix task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,20 @@
 #include "sort.h"
 
 /**
+ * swap - swap two values of an array
+ * @array: array
+ * @i1: index of the first value
+ * @i2: index of the second value
+ */
+void swap(int **array, size_t i1, size_t i2)
+{
+	int tmp;
+
+	tmp = (*array)[i1];
+	(*array)[i1] = (*array)[i2];
+	(*array)[i2] = tmp;
+}
+/**
  * bubble_sort - sorts an array of integers
  * in ascending order using the Bubble sort algorithm
  * @array: array to sort
@@ -9,7 +23,6 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t n = size, newn, i;
-	int tmp;
 
 	if (array == NULL || size < 2)
 		return;
@@ -20,9 +33,7 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[i - 1] > array[i])
 			{
-				tmp = array[i];
-				array[i] = array[i - 1];
-				array[i - 1] = tmp;
+				swap(&array, i - 1, i);
 				print_array(array, size);
 				newn = i;
 			}
