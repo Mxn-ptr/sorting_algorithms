6fcc81adbb3d208c51bea026594be6f86deead86
Mxn-ptr <maxence0@hotmail.fr>
2022-02-14 09:38:43 +0100

    Add task 0 to 2

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers
+ * in ascending order using the Bubble sort algorithm
+ * @array: array to sort
+ * @size: size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t n = size, newn, i;
+	int tmp;
+
+	while (n > 1)
+	{
+		for (i = 1; i <= (n - 1); i++)
+		{
+			if (array[i - 1] > array[i])
+			{
+				tmp = array[i];
+				array[i] = array[i - 1];
+				array[i - 1] = tmp;
+				print_array(array, size);
+				newn = i;
+			}
+		}
+		n = newn;
+	}
+}
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm
+ * @list: list to sort
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+
+	listint_t *node1 = (*list)->next, *node2;
+
+	if (list == NULL || *list == NULL)
+		return;
+
+	while (node1 != NULL)
+	{
+		node2 = node1->next;
+		while (node1->prev != NULL && node1->prev->n > node1->n)
+		{
+			node1->prev->next = node1->next;
+
+			if (node1->next)
+				node1->next->prev = node1->prev;
+
+			node1->next = node1->prev;
+			node1->prev = node1->next->prev;
+			node1->next->prev = node1;
+
+			if (node1->prev == NULL)
+				*list = node1;
+			else
+				node1->prev->next = node1;
+		}
+		node1 = node2;
+		print_list(*list);
+
+	}
+}
